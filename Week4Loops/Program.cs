5f277189e395c929ba3b8e905937f08bcbeb0491
Kaisa Lumiste <lumistekaisa@gmail.com>
2024-09-14 12:47:08 +0300

    week4

---
+++ Week4Loops/Program.cs
@@ -0,0 +1,19 @@
+﻿
+
+Random rnd = new Random();
+
+int myRandom;
+
+
+for (int i = 0; i < 10; i++)
+{
+   myRandom = rnd.Next(1, 11); 
+   Console.WriteLine($"{i + 1}. {myRandom}");
+}
+
+
+
+//for(int i = 0; i < 10; i++) // int++ ==> i + 1
+//{
+//    Console.WriteLine($"{i + 1}. I will not skip Zoom class.");
+//}
---
+++ Week4WhileLoop/Program.cs
@@ -0,0 +1,23 @@
+﻿
+
+Random rnd = new Random();
+int cpuRandom = rnd.Next(1, 4);
+
+bool loopActive = true;
+
+int numberOfTries = 0;
+
+while (numberOfTries < 3)
+{
+    Console.WriteLine("Try to guess the cpu number:");
+    int userGuess = Convert.ToInt32(Console.ReadLine());
+    numberOfTries++;
+
+    if(userGuess == cpuRandom)
+    {
+        Console.WriteLine("You won!");
+        break;
+    }
+}
+Console.WriteLine($"The number of tries: {numberOfTries}");
+Console.WriteLine("Have a nice day!");
